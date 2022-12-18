e7160bf901ac2447ea221bd7372eb00f09ff1153
Aidiakapi <aidiakapi@gmail.com>
2022-12-18 15:06:02 +0100

    Optimize day15.

--- src/day15.rs
+++ src/day15.rs
@@ -1,4 +1,3 @@
-use rayon::prelude::*;
 framework::day!(15, parse => pt1, pt2);
 
 type Vec2 = framework::vecs::Vec2<i32>;
@@ -30,24 +29,48 @@ struct Sensor {
     radius: i32,
 }
 
-fn try_find_defective<const UPPER: i32>(
-    sensors: &[Sensor],
-    y: i32,
-) -> Option<CombiOutput<[i64; 3]>> {
-    let mut x = 0;
-    'outer: while x <= UPPER {
-        for sensor in sensors {
-            let radius = sensor.radius - (sensor.position.y - y).abs();
-            if x >= sensor.position.x - radius && x <= sensor.position.x + radius {
-                x = sensor.position.x + radius + 1;
-                continue 'outer;
-            }
-        }
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+struct Diagonal {
+    x_intercept: i32,
+    from: i32,
+    to: i32,
+}
 
-        let (x, y) = (x as i64, y as i64);
-        return Some(CombiOutput([x, y, x * 4_000_000 + y]));
+impl Diagonal {
+    fn fwd_from_points(a: Vec2, b: Vec2) -> Diagonal {
+        let x_intercept = a.x + a.y;
+        assert_eq!(x_intercept, b.x + b.y);
+        let (from, to) = a.x.minmax(b.x);
+        Diagonal {
+            x_intercept,
+            from,
+            to,
+        }
     }
-    None
+
+    fn bwd_from_points(a: Vec2, b: Vec2) -> Diagonal {
+        let x_intercept = a.y - a.x;
+        assert_eq!(x_intercept, b.y - b.x);
+        let (from, to) = a.x.minmax(b.x);
+        Diagonal {
+            x_intercept,
+            from,
+            to,
+        }
+    }
+
+    fn intersect(fwd: &Diagonal, bwd: &Diagonal) -> Option<Vec2> {
+        let point = Vec2::new(-bwd.x_intercept, bwd.x_intercept) + fwd.x_intercept;
+        if point % 2 != Vec2::zero() {
+            return None;
+        }
+        let point = point / 2;
+        if (fwd.from..=fwd.to).contains(&point.x) && (bwd.from..=bwd.to).contains(&point.x) {
+            Some(point)
+        } else {
+            None
+        }
+    }
 }
 
 fn find_defective<const UPPER: i32>(scans: &[Scan]) -> Result<CombiOutput<[i64; 3]>> {
@@ -60,30 +83,46 @@ fn find_defective<const UPPER: i32>(scans: &[Scan]) -> Result<CombiOutput<[i64; 
         .sorted_by(|a, b| b.radius.cmp(&a.radius))
         .collect_vec();
 
-    const CHUNK_SIZE: i32 = 10_000;
-    (0..UPPER + 1)
-        .step_by(CHUNK_SIZE as usize)
-        .par_bridge()
-        .find_map_first(|y: i32| {
-            let range = y..(y + CHUNK_SIZE).min(UPPER + 1);
-            // Filter sensors down to those that can potentially hit this chunk
-            let sensors = sensors
-                .iter()
-                .filter(|sensor| {
-                    let start = sensor.position.y - sensor.radius;
-                    let end = sensor.position.y + sensor.radius;
-                    start < range.end && range.start <= end
-                })
-                .cloned()
-                .collect_vec();
-            for y in range {
-                if let Some(defective) = try_find_defective::<UPPER>(&sensors, y) {
-                    return Some(defective);
-                }
+    // Forward / diagonals
+    let mut diagonals_fwd = Vec::with_capacity(sensors.len() * 2);
+    // Backward \ diagonals
+    let mut diagonals_bwd = Vec::with_capacity(sensors.len() * 2);
+
+    for sensor in &sensors {
+        let edge = sensor.radius + 1;
+        let lft = sensor.position - Vec2::new(edge, 0);
+        let rgt = sensor.position + Vec2::new(edge, 0);
+        let top = sensor.position - Vec2::new(0, edge);
+        let bot = sensor.position + Vec2::new(0, edge);
+        diagonals_fwd.push(Diagonal::fwd_from_points(bot, rgt));
+        diagonals_fwd.push(Diagonal::fwd_from_points(top, lft));
+        diagonals_bwd.push(Diagonal::bwd_from_points(bot, lft));
+        diagonals_bwd.push(Diagonal::bwd_from_points(top, rgt));
+    }
+
+    let point = diagonals_fwd
+        .iter()
+        .filter_map(|fwd| {
+            for bwd in &diagonals_bwd {
+                if let Some(point) = Diagonal::intersect(fwd, bwd) &&
+                point.x >= 0 && point.y >= 0 &&
+                point.x <= UPPER && point.y <= UPPER &&
+                sensors
+                    .iter()
+                    .all(|s| s.position.manhathan_distance(point) > s.radius) {
+                return Some(point);
+            }
             }
             None
         })
-        .ok_or(Error::NoSolution)
+        .next()
+        .ok_or(Error::NoSolution)?
+        .to_i64();
+    Ok(CombiOutput([
+        point.x,
+        point.y,
+        point.x * 4_000_000 + point.y,
+    ]))
 }
 
 fn pt1(scans: &[Scan]) -> usize {
